7469cf7e42cba6f6e9eca58146bf5674f75ff05f
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-28 00:58:48 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,14 +7,14 @@
  }
 
 def select_winner(passengers)
-  # add the code snippet here!
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with("A")
-      winner = name
-      puts winner
-    end
-    
+# add the code snippet here!
+ winner = ""
+ passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+ winner = name
+  end
 end
+ 
+winner
  select_winner(passengers)
 
