80a04e47f5d75221b9fa62afc0916bc775782426
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-28 01:08:49 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,19 +1,15 @@
- passengers = {
- suite_a: "Amanda Presley", 
- suite_b: "Seymour Hoffman", 
- suite_c: "Alfred Tennyson", 
- suite_d: "Charlie Chaplin", 
- suite_e: "Crumpet the Elf"
- }
-
-def select_winner(passenger)
-  # add the code snippet here!
-  winner = ""
-  passenger.each do |suite, name|
-    if suite == :suite_a && name.start_with("A")
-      winner = name
-    end
-    winner
+passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
+ winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+     winner = name
+  end
 end
-p select_winner(passengers)
-
+ 
+winner
